6f27773cc6aa7df2452adff825c9af48ab8b98bf
shashwatsrii <96522015+shashwatsrii@users.noreply.github.com>
2024-12-03 02:25:01 +0530

    update changes

--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const connectDB = async() => {
-    try{
-        await mongoose.connect(proces.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB connected successfully');
-    } catch(error){
-        console.error('MongoDB connection failed');
-        process.exit(1);
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.log(error);
+    console.error("MongoDB connection failed");
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
